86d4a4728ab5aa4a1d8d74a33617d1421e1f3abc
Jesse Moeller <jessemmoeller@gmail.com>
2024-02-28 21:34:12 -0800

    delete lots of loading by using BC as the swap

--- day_12/omega_hero.asm
+++ day_12/omega_hero.asm
@@ -8,19 +8,12 @@ BLANK .EQU $20
 OMEGA .EQU $CA
 ; Begin Program
 .DB t2ByteTok, tAsmCmp
-; Scratch memory for old cursor position
-OldRow:
-    .DB $00
-OldCol:
-    .DB $00
     bcall(_RunIndicOff)
     bcall(_ClrLCDFull)
     LD  BC, 0
-    LD (IY+(appFlags+appTextSave)), C
+    LD  (IY+(appFlags+appTextSave)), C
     LD  (CurRow), BC
-    LD  (OldRow), BC
-    LD  A, OMEGA
-    bcall(_PutMap)
+    JR  UpdateOmega
 KeyLoop:
     bcall(_GetCSC)
     CP  skRight
@@ -35,42 +28,34 @@ KeyLoop:
     RET Z
     JR  KeyLoop
 Right:
-    LD  A, (CurCol)
+    LD  A, B
     CP  15
     JR  Z, KeyLoop
-    INC A
-    LD  (CurCol), A
+    INC B
     JR  UpdateOmega
 Left:
-    LD  A, (CurCol)
+    LD  A, B
     CP  0
     JR  Z, KeyLoop
-    DEC A
-    LD  (CurCol), A
+    DEC B
     JR  UpdateOmega
 Down:
-    LD  A, (CurRow)
+    LD  A, C
     CP  7
     JR  Z, KeyLoop
-    INC A
-    LD  (CurRow), A
+    INC C
     JR  UpdateOmega
 Up:
-    LD  A, (CurRow)
+    LD  A, C
     CP  0
     JR  Z, KeyLoop
-    DEC A
-    LD  (CurRow), A
+    DEC C
     JR  UpdateOmega
 UpdateOmega:
-    LD  A, OMEGA
-    bcall(_PutMap)
-    LD  BC, (CurRow)
-    LD  HL, (OldRow)
-    LD  (CurRow), HL
     LD  A, BLANK
     bcall(_PutMap)
     LD  (CurRow), BC
-    LD  (OldRow), BC
+    LD  A, OMEGA
+    bcall(_PutMap)
     JR KeyLoop
 .end
